73c9d4c7cd955248e2130c76d7846a579cce47b5
frogbam <frogbam07@gmail.com>
2022-05-03 16:29:18 +0900

    first init

---
+++ pcdet/models/backbones_3d/__init__.py
@@ -0,0 +1,15 @@
+from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
+from .spconv_unet import UNetV2
+from .IASSD_backbone import IASSD_Backbone
+from .IASSD_backbone_fu import IASSD_Backbone_fu
+
+__all__ = {
+    'VoxelBackBone8x': VoxelBackBone8x,
+    'UNetV2': UNetV2,
+    'PointNet2Backbone': PointNet2Backbone,
+    'PointNet2MSG': PointNet2MSG,
+    'VoxelResBackBone8x': VoxelResBackBone8x,
+    'IASSD_Backbone': IASSD_Backbone,
+    'IASSD_Backbone_fu': IASSD_Backbone_fu,
+}
